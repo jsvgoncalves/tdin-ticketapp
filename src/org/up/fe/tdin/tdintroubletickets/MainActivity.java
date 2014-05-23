c87c69c5a3bd87a882b714ec4b955bd08e2dfdbd
Artur Gomes <arturngomes@gmail.com>
2014-05-23 01:35:53 +0100

    Added register and login views
    - Added register view and activity with code to navigate back to login
      screen. (todo: implement registrations)
    - Added minor form validations on registration view
    - Added login view and activity which with needed code to navigate to
        register screen. (todo: implement login)
    - Added style for application buttons (see activity_main.xml to know how
      to change style for new buttons)

--- src/org/up/fe/tdin/tdintroubletickets/MainActivity.java
+++ src/org/up/fe/tdin/tdintroubletickets/MainActivity.java
@@ -3,43 +3,74 @@ package org.up.fe.tdin.tdintroubletickets;
 import org.up.fe.tdin.tdintroubletickets.helper.ComService;
 
 import android.os.Bundle;
-import android.os.Debug;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	ProgressDialog dialog;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
 
-        // Try to login
-        new ComService(
-			"/", 
-			MainActivity.this, 
-			"loginDone",
-			false
-        ); // Hide progress bar cuz we will set it manually
-	
-        
-    }
-    
-    public void loginDone(String result) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// Try to login //Crashing here
+		/*try {
+			new ComService(
+					"/", 
+					MainActivity.this, 
+					"loginDone",
+					false
+					); // Hide progress bar cuz we will set it manually
+		} catch (Exception e) {
+			Toast toast = Toast.makeText(MainActivity.this, getString(R.string.connectionexception), Toast.LENGTH_SHORT);
+			toast.show();
+		}*/
+		addListeners();
+	}
+
+	/**
+	 * Add listeners for button clicks (Login and register)
+	 */
+	private void addListeners() {
+		//button used navigate to registration activity
+		Button register_btn = (Button)findViewById(R.id.button_register);
+		register_btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent register_intent = new Intent(MainActivity.this, RegisterActivity.class);
+				startActivity(register_intent);
+			}
+		});
+
+		//button used to login and navigate to main activity
+		Button login_btn = (Button)findViewById(R.id.button_login);
+		login_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//TODO: Login on service
+			}
+		});
+
+	}
+
+	public void loginDone(String result) {
 		Log.d("loginDone()", "I've logged in");
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
---
+++ src/org/up/fe/tdin/tdintroubletickets/RegisterActivity.java
@@ -0,0 +1,83 @@
+package org.up.fe.tdin.tdintroubletickets;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		addListeners();
+	}
+
+	/**
+	 * Add Listeners to buttons in view
+	 */
+	private void addListeners() {
+
+
+		//Textviews that contain all the data
+		final TextView name_txt = (TextView)findViewById(R.id.form_name);
+		final TextView email_txt = (TextView) findViewById(R.id.form_email);
+		final TextView password_txt = (TextView) findViewById(R.id.form_pw);
+		final TextView password_repeat_txt = (TextView) findViewById(R.id.form_pw2);
+
+		//button used to create account
+		Button create_btn = (Button)findViewById(R.id.button_createAccount);
+		create_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//TODO: Register account on server
+				if(name_txt.getText().toString().length() == 0){
+					showError(getString(R.string.form_error_no_user)); //empty user name
+				}else if(!isValidEmail(email_txt.getText().toString())){
+					showError(getString(R.string.form_error_invalid_email)); //invalid email
+				}else if(!password_txt.getText().toString().equals(password_repeat_txt.getText().toString())){
+					showError(getString(R.string.form_error_diff_pass)); //show different passwords error
+				} 
+			}
+		});
+
+		//buton used to cancel account registration
+		Button cancel_btn = (Button)findViewById(R.id.button_cancel_register);
+		cancel_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+	}
+
+	/*
+	 * @source: http://stackoverflow.com/questions/1819142/how-should-i-validate-an-e-mail-address-on-android
+	 */
+	private boolean isValidEmail(CharSequence target) {
+		return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+	}
+
+	/**
+	 * Show error message for 3 seconds fadding in and out
+	 * @param error_msg -> the error message to be displayed
+	 */
+	private void showError(final String error_msg){
+		final LinearLayout error_layout = (LinearLayout) findViewById(R.id.error_lay);
+		final TextView error_text = (TextView) findViewById(R.id.register_error);
+		error_text.setText(error_msg);
+		AlphaAnimation anim = new AlphaAnimation(1.0f, 0.0f);
+		anim.setDuration(3000);
+		anim.setRepeatMode(Animation.REVERSE);
+		error_layout.startAnimation(anim);
+	}
+
+}
