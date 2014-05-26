e7c8a7cce737e167f88852e7dbd033b0816f8b26
João Gonçalves <jsvgoncalves@gmail.com>
2014-05-26 13:29:27 +0100

    Added Application class.
    Will be used to store preferences. Also added it to AndroidManifest.xml.

---
+++ src/org/up/fe/tdin/model/TDINTroubleTickets.java
@@ -0,0 +1,14 @@
+package org.up.fe.tdin.tdintroubletickets.model;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class TDINTroubleTickets extends Application {
+
+
+
+}
