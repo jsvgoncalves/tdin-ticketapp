4d0923b2fe9655e543ad4a8cd1198c692088124f
João Gonçalves <jsvgoncalves@gmail.com>
2014-05-27 10:17:05 +0100

    Added tickets list to the view.
    Still needs to retrieve the list from the server.

--- src/org/up/fe/tdin/tdintroubletickets/HomeActivity.java
+++ src/org/up/fe/tdin/tdintroubletickets/HomeActivity.java
@@ -1,5 +1,10 @@
 package org.up.fe.tdin.tdintroubletickets;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.up.fe.tdin.tdintroubletickets.model.TDINTroubleTickets;
 import org.up.fe.tdin.tdintroubletickets.helper.ComService;
 import org.up.fe.tdin.tdintroubletickets.helper.JSONHelper;
 
@@ -11,6 +16,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -18,10 +27,17 @@ import org.json.JSONObject;
 
 public class HomeActivity extends Activity {
 
+	TDINTroubleTickets tdin;
+	ArrayList<Map<String,String>> ticketsArray;
+	private SimpleAdapter simpleAdpt;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		tdin = (TDINTroubleTickets) getApplication();
 		setContentView(R.layout.activity_home);
+		super.onCreate(savedInstanceState);
+
+		initList();
 	}
 
 	@Override
@@ -31,6 +47,28 @@ public class HomeActivity extends Activity {
 		return true;
 	}
 
-	
+	private void initList() {
+		ticketsArray = new ArrayList<Map<String,String>>();
+		HashMap<String, String> ticketItem = new HashMap<String, String>();
+		ticketItem.put("first-line", "No tickets");
+		ticketItem.put("second-line", "You have not been assigned any trouble tickets.");
+		ticketsArray.add(ticketItem);
+
+		ListView lv = (ListView) findViewById(R.id.history_list_view);
+		simpleAdpt = new SimpleAdapter(this, ticketsArray, android.R.layout.simple_list_item_2,
+				new String[] {"first-line", "second-line"}, new int[] {android.R.id.text1,android.R.id.text2});
+
+		lv.setAdapter(simpleAdpt);
+
+		// Create a message handling object as an anonymous class.
+		OnItemClickListener mMessageClickedHandler = new OnItemClickListener() {
+			public void onItemClick(AdapterView parent, View v, int position, long id) {
+				// Do something in response to the click
+				// Log.e("mylog", "clicked a ticket " + position);
+			}
+		};
+
+		lv.setOnItemClickListener(mMessageClickedHandler);
+	}
 
 }
